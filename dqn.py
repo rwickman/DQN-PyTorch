Fix training loop and computing gradients

dqn.py
@@ -13,12 +13,13 @@ EPS_START = 0.9
 EPS_END = 0.05
 
 class DQNActor:
-    def __init__(self, args):
+    def __init__(self, args, action_dim):
         self._args = args
-        self._dqnet = DQNetwork()
-        self._dqnet_target = DQNetwork()
+        self._action_dim = action_dim
+        self._dqnet = DQNetwork(args, self._action_dim)
+        self._dqnet_target = DQNetwork(args, self._action_dim)
         self._replay_memory = ReplayMemory(args)
-        self._optimizer = optim.RMSprop(self._dqnet.parameters(), lr=args.lr)
+        self._optimizer = optim.Adam(self._dqnet.parameters(), lr=self._args.lr)
         self._epsilon = args.epsilon
         self._loss_fn = nn.MSELoss()
         self._num_steps = 0
@@ -31,9 +32,9 @@ class DQNActor:
     
     def get_action(self, q_values, argmax=False):
         q_values = torch.squeeze(q_values)
-        if argmax or self.epsilon_threshold() >= np.random.rand():
+        if not argmax and self.epsilon_threshold() >= np.random.rand():
             # Perform random action
-            action = np.random.randint(1)
+            action = np.random.randint(self._action_dim)
         else:
             with torch.no_grad():
                 # Perfrom action that maximizes expected return
@@ -47,8 +48,8 @@ class DQNActor:
         exs = self._replay_memory.sample()
     
         td_targets = torch.zeros(self._args.batch_size)
-        states = torch.zeros(self._args.batch_size, 2, 84, 84)
-        next_states = torch.zeros(self._args.batch_size, 2, 84, 84)
+        states = torch.zeros(self._args.batch_size, 1, self._args.img_dim, self._args.img_dim)
+        next_states = torch.zeros(self._args.batch_size, 1, self._args.img_dim, self._args.img_dim)
         rewards = torch.zeros(self._args.batch_size)
         next_state_mask = torch.zeros(self._args.batch_size)
         actions = []
@@ -60,14 +61,13 @@ class DQNActor:
             if e_t.next_state is not None:
                 next_states[i] = e_t.next_state 
                 next_state_mask[i] = 1
-                # _, next_q_value = self.get_action(
-                #     self._dqnet_target(e_t.next_state),
-                #     True)
-                # td_targets[i] = e_t.reward + self._args.gamma * next_q_value
         
         # Select the q-value for every state
-        q_values = torch.Tensor(
-            [q[actions[i]] for i, q in enumerate(self._dqnet(states))])
+        #print(actions)
+        actions = torch.tensor(actions, dtype=torch.int64)
+        q_values = self._dqnet(states).gather(1, actions.unsqueeze(0))
+        #torch.Tensor([q[actions[i]] for i, q in enumerate(self._dqnet(states))])
+        
         q = self._dqnet_target(next_states) 
         for i in range(self._args.batch_size):
             if next_state_mask[i] == 0:
@@ -76,29 +76,23 @@ class DQNActor:
                 _, next_q_value = self.get_action(q[i], True)
                 td_targets[i] = rewards[i] + self._args.gamma * next_q_value 
 
-            
 
         # Train model
+        self._optimizer.zero_grad()
         loss = self._loss_fn(q_values, td_targets)
         loss.backward()
         self._optimizer.step()
-        self._optimizer.zero_grad()
-
         self._num_steps += 1
         
         # Update target policy
         if (self._num_steps + 1) % self._args.target_update_step == 0:
-            
+            print("loss", loss)
             print("epsilon_threshold", self.epsilon_threshold())
             print("q_values", q_values)
             print("td_targets", td_targets)
             print("replay_len", self.replay_len())
             self._dqnet_target.load_state_dict(self._dqnet.state_dict())
             #target_net.load_state_dict(policy_net.state_dict())
-
-    def copy_to_target(self):
-        """Copy to parameters to target network."""
-        pass
         
     def add_ex(self, e_t):
         """Add a step of experience."""
@@ -110,7 +104,6 @@ class DQNActor:
     def epsilon_threshold(self):
         return self._args.min_epsilon + (self._args.epsilon - self._args.min_epsilon) * \
             math.exp(-1. * self._num_steps / self._args.epsilon_decay)
-        #return f_1 * f_2
 
     def save(self):
         torch.save(self._dqnet.state_dict(), self._args.model)
@@ -126,15 +119,21 @@ class DQNActor:
                 self._num_steps = d["num_steps"]
 
 class DQNetwork(nn.Module):
-    def __init__(self, action_dim=2):
+    def __init__(self, args, action_dim=2):
         super().__init__()
         # 4 input frames, 16 filters of size 8x8
-        self.conv1 = nn.Conv2d(2, 16, 8, stride=4)
+        self.conv1 = nn.Conv2d(1, 16, 8, stride=4)
         # 32 filter of size 4x4
         self.conv2 = nn.Conv2d(16, 32, 4, stride=2)
         self.flatten = nn.Flatten()
-        # 256 units 
-        self.fc1 = nn.Linear(32 * 9 * 9, 256)
+        
+        
+        conv2d_out_dim = self._conv2d_out(
+            self._conv2d_out(args.img_dim, 8, 4), 4, 2)        
+        num_units = int(conv2d_out_dim ** 2 * 32)
+        print("conv2d_out_dim", conv2d_out_dim)
+        print("num_units", num_units)
+        self.fc1 = nn.Linear(num_units, 256)
         self.fc2 = nn.Linear(256, action_dim)
 
     
@@ -146,5 +145,8 @@ class DQNetwork(nn.Module):
         # Outputs state-action value q(s,a) for every action
         return self.fc2(x)
     
+    def _conv2d_out(self, dim, kernel_size, stride):
+        # W - F + 2P / S
+        return (dim - kernel_size) / stride + 1
     # def action(self, x):
     #     if np.random.rand() > 

replay_memory.py
@@ -1,4 +1,4 @@
-import random
+import numpy as np
 import torch
 from dataclasses import dataclass
 
@@ -29,7 +29,7 @@ class ReplayMemory:
 
     def sample(self):
         """Sample batch size experience replay."""
-        return random.sample(self._memory, self._args.batch_size)
+        return np.random.choice(self._memory, size=self._args.batch_size, replace=False)
 
     def current_capacity(self):
         return len(self._memory)

trainer.py
@@ -15,62 +15,63 @@ class Trainer:
         self._env = env
         self._actor = actor
         
-        
-    
     def current_frame(self):
-        # Get current frame
+        """Get current frame."""
+        # Get frame
         frame = self._env.render(mode='rgb_array')
+
         # Convert frame to grayscale
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        # Resize the frame
-        frame = cv2.resize(frame, (84,84), interpolation = cv2.INTER_LINEAR)
         
-        return torch.from_numpy(frame)
+        # Resize the frame
+        frame = cv2.resize(frame, (self._args.img_dim, self._args.img_dim), interpolation = cv2.INTER_LINEAR)
+        
+        # Normalize frame to [-1, 1]
+        frame = frame / 127.5 - 1
+        
+        return torch.from_numpy(frame).float().unsqueeze(0).unsqueeze(0)
 
 
     def run(self):
         self._env.reset()
         for e_i in range(self._args.episodes):
-            s_i = 0
             done = False
-            first_state = True
+            total_reward = 0
+            cur_frame = self.current_frame()
+            state = cur_frame
             while not done:
-                s_i += 1
-                state = torch.zeros(1, 2, 84, 84)
-                # Add current frame to state
-                #state[:, 0] = current_frame()
-                cur_reward = 0
-                for i in range(2):
-                    #print(frame.shape)
-                    if first_state:
-                        # Don't do anything
-                        self._env.step(DO_NOTHING_ACTION)
-                    else:
-                        _, reward, done, _ = self._env.step(action)
-                        cur_reward += reward
-                    state[:, i] = self.current_frame()
-                    #print(frame.shape)
-                    
-                    #plt.imshow(frame, cmap='gray', vmin=0, vmax=255)
-                    #plt.show()
+                # Get current action
+                action, _ = self._actor(state)
                 
-                action, q_value = self._actor(state)
+                # Perform action in environment
+                _, reward, done, _ = self._env.step(action)
                 
-                if first_state:
-                    first_state = False
-                else:   
-                    # Add memory step
-                    if done:
-                        state = None
-                    e_t = Experience(prev_state, action, reward, state)
-                    self._actor.add_ex(e_t)
+                # Update frames
+                prev_frame = cur_frame
+                cur_frame = self.current_frame()
 
+                # Get next state
+                next_state = cur_frame - prev_frame
+               
+                # Add memory step
+                if done:
+                    next_state = None
+                    print(reward, action)
+                total_reward += reward
+                e_t = Experience(state, action, reward, next_state)
+                self._actor.add_ex(e_t)
+                state = next_state 
+
+                # plt.imshow(state[0].transpose(0,2).transpose(0,1))
+                # plt.show()
+                
                 if e_i > 0 and self._actor.replay_len() > self._args.batch_size:
                     #print(self._actor.replay_len())
                     self._actor.train()
 
                 prev_state = state
-            print("Episode", e_i, "steps", s_i)
+                prev_frame = cur_frame
+            print("Episode", e_i, "total reward", total_reward)
             self._env.reset()
             self._actor.save()
 